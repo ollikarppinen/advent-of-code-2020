9b562ceaaf267f2bee0d3ef5366eca80fd9c5937
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-05 23:52:14 +0200

    Improves solution

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,33 @@ use std::path::Path;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+const TARGET: i32 = 2020;
+
 fn main() {
-    let ls = lines_from_file("./day-1-input.txt");
-    'outer: for l in &ls {
-        for ll in &ls {
-            let l = l.parse::<i32>().unwrap();
-            let ll =  ll.parse::<i32>().unwrap();
-            if l + ll == 2020 {
-                println!("{} + {} = 2020", l, ll);
-                println!("{} * {} = {}", l, ll, l * ll);
-                break 'outer;
+    let mut ls = ints_from_file("./day-1-input.txt");
+    ls.sort();
+    'i: for i in 0..ls.len() {
+        'j: for j in i..ls.len() {
+            let l1 = ls[i];
+            let l2 = ls[j];
+            let sum = ls[i] + ls[j];
+            println!("{}, {}", l1, l2);
+            if sum == TARGET {
+                println!("{} * {} = {}", l1, l2, l1 * l2);
+                break 'i;
+            } else if sum > TARGET {
+                break 'j;
             }
         }
     }
 }
 
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+fn ints_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
     buf.lines()
         .map(|l| l.expect("Could not parse line"))
+        .map(|x| x.parse::<i32>().unwrap())
         .collect()
 }
 
