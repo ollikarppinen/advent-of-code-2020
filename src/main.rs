ed063cb4c1ea332b3eb3b4aba95fc758ef9a0a10
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-06 12:24:24 +0200

    Adds day1 function

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,9 @@ use std::path::Path;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-const TARGET: i32 = 2020;
 
 fn main() {
-    let mut ls = ints_from_file("./day-1-input.txt");
-    ls.sort();
-    'i: for i in 0..ls.len() {
-        let a = ls[i];
-        for j in i..ls.len() {
-            let b = ls[j];
-            'k: for k in j..ls.len() {
-                let c = ls[k];
-                let sum = a + b + c;
-                if sum == TARGET {
-                    println!("{} * {} * {} = {}", a, b, c, a * b * c);
-                    break 'i;
-                } else if sum > TARGET {
-                    break 'k;
-                }
-            }
-        }
-    }
+    day1();
 }
 
 fn ints_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
@@ -33,5 +15,27 @@ fn ints_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
         .map(|l| l.expect("Could not parse line"))
         .map(|x| x.parse::<i32>().unwrap())
         .collect()
+}
+
+fn day1 () -> () {
+    let target = 2020;
+    let mut ls = ints_from_file("./day-1-input.txt");
+    ls.sort();
+    'i: for i in 0..ls.len() {
+        let a = ls[i];
+        for j in i..ls.len() {
+            let b = ls[j];
+            'k: for k in j..ls.len() {
+                let c = ls[k];
+                let sum = a + b + c;
+                if sum == target {
+                    println!("{} * {} * {} = {}", a, b, c, a * b * c);
+                    break 'i;
+                } else if sum > target {
+                    break 'k;
+                }
+            }
+        }
+    }
 }
 
