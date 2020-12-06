2c8ea1e85d6325653fc9b3490c20507e3b5e0ea6
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-06 13:25:26 +0200

    Adds day 2 1 start solution

--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,30 @@ fn day1 () -> () {
 }
 
 fn day2 () -> () {
-    let lines = lines_from_file("./day-2-input.txt");
-    for line in lines {
-        let words: Vec<&str> = line.split_whitespace().collect();
-        let min_max: Vec<i32> = words[0].split("-").collect::<Vec<&str>>().iter().map(|line| line.parse::<i32>().unwrap()).collect();
-        let chr = &words[1][0..1];
-        let pw = words[2];
-        println!("min_max: {:?}, chr: {}, pw: {}", min_max, chr, pw);
-    }
+    let valid_password_count = lines_from_file("./day-2-input.txt").iter().map(day_2_parse_line).filter(password_valid).count();
+    println!("Valid password count: {:#?}", valid_password_count);
 }
 
-fn 
+fn password_valid(passwords_and_restrictions: &PasswordAndRestrictions) -> bool {
+    let substring_count = passwords_and_restrictions.password.matches(&passwords_and_restrictions.substring).count();
+    substring_count >= passwords_and_restrictions.min_substring as usize && substring_count <= passwords_and_restrictions.max_substring as usize
+}
+
+#[derive(Debug)]
+struct PasswordAndRestrictions {
+    password: String,
+    substring: String,
+    min_substring: i32,
+    max_substring: i32
+}
+
+fn day_2_parse_line(line: &String) -> PasswordAndRestrictions {
+    let words: Vec<&str> = line.split_whitespace().collect();
+    let min_max: Vec<i32> = words[0].split("-").collect::<Vec<&str>>().iter().map(|line| line.parse::<i32>().unwrap()).collect();
+    let substring = &words[1][0..1];
+    let password = words[2];
+    PasswordAndRestrictions { password: password.to_string(), substring: substring.to_string(), min_substring: min_max[0], max_substring: min_max[1] }
+}
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
