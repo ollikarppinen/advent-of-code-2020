512c61bef52c550f2e7a27ac6b3943f59c2bc28d
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-07 23:21:02 +0200

    Adds byr validation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+extern crate regex;
+
 mod helpers;
 mod solutions;
 
 fn main() {
     // solutions::day1::run();
     // solutions::day2::run();
-    // solutions::day3::run();
+    //solutions::day3::run();
     solutions::day4::run();
 }
--- src/solutions/day4.rs
+++ src/solutions/day4.rs
@@ -1,8 +1,10 @@
 use helpers;
 use std::path::Path;
+use regex::Regex;
 
 pub fn run () -> () {
-    let passports = parse_passports("./day-4-input.txt");
+    println!("Regex match: {}", matches_regex(r"^\d{4}$", "2014"));
+    let passports = parse_passports("./day-4-example-input.txt");
     let valid_passport_count = passports.iter().filter(&&valid_passport).count();
     println!("Day 4, part 1: valid passport count: {:?}", valid_passport_count);
 }
@@ -20,15 +22,54 @@ struct Passport {
 }
 
 fn valid_passport(passport: &&Passport) -> bool {
-    !(
-        passport.byr.is_none() ||
-        passport.iyr.is_none() ||
-        passport.eyr.is_none() ||
-        passport.hgt.is_none() ||
-        passport.hcl.is_none() ||
-        passport.ecl.is_none() ||
-        passport.pid.is_none()
-    )
+    byr_valid(passport.byr.as_ref()) &&
+    iyr_valid(passport.iyr.as_ref()) &&
+    eyr_valid(passport.eyr.as_ref()) &&
+    hgt_valid(passport.hgt.as_ref()) &&
+    hcl_valid(passport.hcl.as_ref()) &&
+    ecl_valid(passport.ecl.as_ref()) &&
+    pid_valid(passport.pid.as_ref()) &&
+    cid_valid(passport.cid.as_ref())
+}
+
+fn byr_valid(byr: Option<&String>) -> bool {
+    if byr.is_none() && matches_regex(r"^\d{4}$", byr.unwrap()) { return false }
+    let byr_int = byr.unwrap().parse::<i32>().unwrap();
+    return byr_int >= 1920 && byr_int <= 2002;
+}
+
+fn iyr_valid(iyr: Option<&String>) -> bool {
+    if iyr.is_none() && matches_regex(r"^\d{4}$", iyr.unwrap()) { return false }
+    return true;
+}
+
+fn eyr_valid(eyr: Option<&String>) -> bool {
+    return !eyr.is_none()
+}
+
+fn hgt_valid(hgt: Option<&String>) -> bool {
+    return !hgt.is_none()
+}
+
+fn hcl_valid(hcl: Option<&String>) -> bool {
+    return !hcl.is_none()
+}
+
+fn ecl_valid(ecl: Option<&String>) -> bool {
+    return !ecl.is_none()
+}
+
+fn pid_valid(pid: Option<&String>) -> bool {
+    return !pid.is_none()
+}
+
+fn cid_valid(_cid: Option<&String>) -> bool {
+    return true
+}
+
+fn matches_regex(pattern: &str, string: &str) -> bool {
+    let re = Regex::new(pattern).unwrap();
+    return re.is_match(string);
 }
 
 fn parse_passports(path: impl AsRef<Path>) -> Vec<Passport> {
