3c8f5b5d766bdb0a1c1d8986c41ef8f361ccecfb
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-07 20:05:15 +0200

    Adds day 3 part 2 solution

--- src/main.rs
+++ src/main.rs
@@ -49,12 +49,15 @@ fn day3 () -> () {
         if chars[xs[1] % line.len()] == '#' { counts[1] += 1 }
         if chars[xs[2] % line.len()] == '#' { counts[2] += 1 }
         if chars[xs[3] % line.len()] == '#' { counts[3] += 1 }
-        if chars[xs[4] % line.len()] == '#' && i % 2 == 1 { counts[4] += 1 }
+        if i % 2 == 0 {
+            if chars[xs[4] % line.len()] == '#' { counts[4] += 1 }
+            xs[4] += 1;
+        }
         xs[0] += 1;
         xs[1] += 3;
         xs[2] += 5;
         xs[3] += 7;
-        xs[4] += 1;
+       
         i += 1;
     }
     println!("Day 3, part 1: {:?}", counts);
