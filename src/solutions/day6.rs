c97e493765d466731a62903f6255f45c53c4e215
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-14 21:25:45 +0200

    Adds day 6 part 2 solution

--- src/solutions/day6.rs
+++ src/solutions/day6.rs
@@ -1,24 +1,34 @@
 use helpers;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 pub fn run () -> () {
     let lines = helpers::lines_from_file("./day-6-input.txt");
     let mut line_iter = lines.iter();
     let mut line: Option<&String>;
-    let mut grouped_lines: Vec<String> = vec![];
-    let mut answers = 0;
+    let mut unique_answers: Option<HashSet::<char>> = None;
+    let mut shared_answers: Option<HashSet::<char>> = None;
+    let mut unique_answer_count = 0;
+    let mut shared_answer_count = 0;
     loop {
         line = line_iter.next();
         if line.is_none() || line.unwrap().len() == 0 {
-            let unique_answers = grouped_lines.join("").chars().collect::<Vec<char>>().iter().unique().count();
-            answers += unique_answers;
-            grouped_lines = vec![];
+            unique_answer_count += unique_answers.unwrap().len();
+            shared_answer_count += shared_answers.unwrap().len();
+            shared_answers = None;
+            unique_answers = None;
         } else {
-            grouped_lines.push(line.unwrap().to_string());
+            if shared_answers.is_none() || unique_answers.is_none() {
+                shared_answers = Some(line.unwrap().chars().collect());
+                unique_answers = Some(line.unwrap().chars().collect());
+            } else {
+                let answers = line.unwrap().chars().collect::<HashSet<char>>();
+                shared_answers = Some(shared_answers.unwrap().intersection(&answers).cloned().collect());
+                unique_answers = Some(unique_answers.unwrap().union(&answers).cloned().collect());
+            }
         }
-        if line.is_none() {
-            break;
-        }
+        if line.is_none() { break; }
     }
-    println!("Day 6, part 1, unique asnwers: {}", answers);
+    println!("Day 6, part 1, unique answers: {}", unique_answer_count);
+    println!("Day 6, part 2, shared answers: {}", shared_answer_count);
 }
