a1701a03af6f1c6e3a4cf47b3f3ea98538518a00
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-21 21:40:50 +0200

    Adds day 7 part 2 solution

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -5,23 +5,31 @@ use regex::Regex;
 
 pub fn run () -> () {
     let lines = helpers::lines_from_file("./day-7-input.txt");
-    let mut rules: HashMap<String, Vec<String>> = HashMap::new();
     let mut results: HashSet::<String> = HashSet::new();
+    let mut rules: HashMap::<String, Vec<(String, i64)>> = HashMap::new();
+    let mut color_to_count: HashMap::<String, i64> = HashMap::new();
     results.insert("shiny gold".to_string());
+
     let rgx = Regex::new(r"(\d*) (\w* \w*) bags{0,1}[\.,]").unwrap();
     for line in lines {
         let splits = line.split(" bags contain ").collect::<Vec<&str>>();
+        let mut vec: Vec<(String, i64)> = vec![];
+        for capture in rgx.captures_iter(splits[1]) {
+            vec.push((capture[2].to_string(), capture[1].parse::<i64>().unwrap()));
+        }
+        if vec.is_empty() { color_to_count.insert(splits[0].to_string(), 1); }
         rules.insert(
             splits[0].to_string(),
-            rgx.captures_iter(splits[1]).map(|capture| capture[2].to_string()).collect()
+            vec
         );
     }
+
     loop {
         let mut retry = false;
         'mid: for (key, xs) in rules.iter() {
             if results.contains(key) { continue 'mid; }
             for x in xs {
-                if results.contains(x) {
+                if results.contains(&x.0) {
                     results.insert(key.to_string());
                     retry = true;
                 }
@@ -30,6 +38,23 @@ pub fn run () -> () {
 
         if !retry { break; }
     }
-    
+
+    loop {
+        let mut retry = false;
+        'mid: for (key, xs) in rules.iter() {
+            if color_to_count.contains_key(key) { continue 'mid; }
+            let mut sum = 1;
+            for x in xs {
+                if !color_to_count.contains_key(&x.0) { continue 'mid; }
+                sum += x.1 * color_to_count.get(&x.0).unwrap();
+            }
+            color_to_count.insert(key.to_string(), sum);
+            retry = true;
+        }
+
+        if !retry { break; }
+    }
     println!("Day 7, part 1: {:?}", results.len() - 1);
+    println!("Day 7, part 2: {:?}", color_to_count.get("shiny gold").unwrap() - 1);
 }
+
