9edf1e61e573e3836ba54000bcfaa0a9e1dded51
Olli Karppinen <ollikarppinen@gmail.com>
2020-12-22 23:58:31 +0200

    Adds day 8 part 2 solution

--- src/solutions/day8.rs
+++ src/solutions/day8.rs
@@ -4,21 +4,46 @@ use std::collections::HashSet;
 pub fn run () -> () {
     let lines = helpers::lines_from_file("./day-8-input.txt");
     let mut i = 0;
+    let mut mutation = 0;
+    let mut mutation_i = 0;
     let mut acc = 0;
     let mut is: HashSet::<i32> = HashSet::new();
+    let line_count = lines.len();
+    let mut mutated = false;
     loop {
-        is.insert(i);
         let splits = lines[i as usize].split(" ").collect::<Vec<&str>>();
-        let cmd = splits[0];
+        let mut cmd = splits[0];
+        if !mutated && cmd == "jmp" || cmd == "nop" {
+            if mutation == mutation_i {
+                println!("Mutation: {} / {}", mutation, line_count);
+                println!("i: {}", i);
+                cmd = if cmd == "jmp" { "nop" } else { "jmp" };
+                mutated = true;
+            }
+            mutation_i += 1;
+        }
         let arg = splits[1].parse::<i32>().unwrap();
+        if i == lines.len() as i32 - 1 {
+            println!("Last line!");
+            acc += if cmd == "acc" { arg } else { 1 };
+            break;
+        }
         i += if cmd == "jmp" { arg } else { 1 };
         if is.contains(&i) {
-            break;
+            is = HashSet::new();
+            mutated = false;
+            mutation += 1;
+            mutation_i = 0;
+            i = 0;
+            acc = 0;
         } else {
             is.insert(i);
         }
-        if i >= lines.len() as i32 { break; }
+        if i >= lines.len() as i32 {
+            println!("i out of bounds!");
+            break;
+        }
         if cmd == "acc" { acc += arg; }
     }
-    println!("Day 8, part 1: {}", acc);
+    println!("Day 8, part 2: {}", acc);
 }
